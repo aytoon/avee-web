de4c496b26b96fb09b25b672d615c18bc849225a
Kai Buss <aveeitstrue@gmail.com>
2022-05-23 18:33:17 +0200

    Uploaded files for open source

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Statics
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/img', express.static(__dirname + 'public/img'))
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+// Navigation
+app.get('/', (req, res) => {
+  res.render('index.ejs')
+})
+
+// app.get('/about', (req, res) => {
+//  res.sendFile(__dirname + '/views/about.html')
+// })
+
+// Listen Port
+app.listen(port, () => {
+  console.info(`☕ Website is going to a coffee at port ${port}`)
+})
